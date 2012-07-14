bf6ca03100201ce1fbb73cb186d8912a50cc74b3
Junghoon Kim <ssuxo.private@gmail.com>
2012-07-15 03:15:53 +0900

    cras

--- WebProject/WebContent/NewFile.jsp
+++ WebProject/WebContent/NewFile.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-ffff
+fixedfff
 </body>
 </html>
--- WebProject/src/com/common/action/CommonAction.java
+++ WebProject/src/com/common/action/CommonAction.java
@@ -1,5 +1,8 @@
 package com.common.action;
 
 public class CommonAction {
-	int i=0;
+	int i=2;
+	int qi=3;
+	String j="aaa";
+	
 }
